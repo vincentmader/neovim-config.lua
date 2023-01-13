e33e917614883dda12bdcd7b9aefeaef39dc56ec
Vincent C. Mader <vincent.mader@icloud.com>
2023-01-13 19:22:48 +0100

    configured tabs: Don't use tabs at all. -> Use 4 spaces instead.

--- lua/general.lua
+++ lua/general.lua
@@ -21,3 +21,11 @@ vim.cmd([[set viminfo+=n~/.local/share/nvim/shada/main.shada]])
 vim.opt.swapfile = false
 
 -- }}}
+-- Tabs {{{
+
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+
+-- }}}
