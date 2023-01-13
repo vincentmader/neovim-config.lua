10af9170fcd8cca35d3915fde6729d3a52c36af0
Vincent C. Mader <vincent.mader@icloud.com>
2023-01-13 19:21:04 +0100

    configured nvim yank & paste to use system clipboard

--- lua/general.lua
+++ lua/general.lua
@@ -1,3 +1,15 @@
+-- Clipboard for Copy & Paste {{{
+
+vim.cmd([[
+    if has("clipboard")
+        set clipboard=unnamed
+        if has("unnamedplus")  " -> X11 support
+            set clipboard+=unnamedplus
+        endif
+    endif
+]])
+
+-- }}}
 -- Shared Data & Swap Files {{{
 
 vim.cmd([[set viminfo+=n~/.local/share/nvim/shada/main.shada]])                                            
