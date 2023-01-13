d256e10edd549b5f7116dcbfe3227bad5039bf4f
Vincent C. Mader <vincent.mader@icloud.com>
2023-01-14 00:41:51 +0100

    defined key mapping for switching between buffers (`gn` and `gp`)

--- lua/navigation.lua
+++ lua/navigation.lua
@@ -9,3 +9,10 @@ vim.opt.splitbelow = true
 vim.opt.splitright = true
 
 -- }}}
+-- Buffers {{{
+
+-- Switch between buffers (and use buffers, not tabs).
+vim.keymap.set('n', 'gn', ':bn<CR>', expr_opts)
+vim.keymap.set('n', 'gp', ':bp<CR>', expr_opts)
+
+-- }}}
