362834d8a0d8b905ba16cb7513ea52f39683a719
Vincent C. Mader <vincent.mader@icloud.com>
2023-01-13 19:28:03 +0100

    added plugin-manager ("wbthomason/packer.nvim")

---
+++ lua/plugins.lua
@@ -0,0 +1,9 @@
+vim.cmd([[
+    if empty(glob("$XDG_DATA_HOME/nvim/site/pack"))
+        !git clone --depth 1 https://github.com/wbthomason/packer.nvim $XDG_DATA_HOME/nvim/site/pack/packer/start/packer.nvim
+    endif
+]])
+
+return require("packer").startup(function() 
+    use "wbthomason/packer.nvim"
+end)
