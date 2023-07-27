8fee77d71cb3b35b48f3b46651b8b12c1e5ed638
Vincent C. Mader <vincent.mader@icloud.com>
2023-07-28 01:11:26 +0200

    MINOR: Add indentation

--- bin/install_coc_plugins.sh
+++ bin/install_coc_plugins.sh
@@ -2,24 +2,25 @@
 
 nvim\
     -u ../init.lua\
-    -c "CocInstall\
-        coc-css\
-        coc-flutter\
-        coc-html\
-        coc-julia\
-        coc-json\
-        coc-pairs\
-        coc-pyright\
-        coc-rust-analyzer\
-        coc-snippets\
-        coc-tsserver\
-        coc-vimtex\
-    "
-        # coc-cl,  # Common Lisp LSP
-        # coc-docker\
-        # coc-eslint\
-        # coc-lua\       -> leads to massive amounts of RAM consumption
-        # coc-jedi\
-        # coc-prettier\
-        # coc-python
-        # coc-sourcekit # Swift LSP
+    -c "\
+        CocInstall\
+            coc-css\
+            coc-flutter\
+            coc-html\
+            coc-julia\
+            coc-json\
+            coc-pairs\
+            coc-pyright\
+            coc-rust-analyzer\
+            coc-snippets\
+            coc-tsserver\
+            coc-vimtex\
+        "
+          # coc-cl,  # Common Lisp LSP
+          # coc-docker\
+          # coc-eslint\
+          # coc-lua\       -> leads to massive amounts of RAM consumption
+          # coc-jedi\
+          # coc-prettier\
+          # coc-python
+          # coc-sourcekit # Swift LSP
