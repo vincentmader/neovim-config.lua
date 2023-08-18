004bc79a592480b704300c3c6213ab9679f21014
Vincent C. Mader <vincent.mader@icloud.com>
2023-08-18 10:14:50 +0200

    FEAT: Add install script for "packer" (plugin manager)

---
+++ plugin/vim-which-key.lua
@@ -0,0 +1,32 @@
+-- vim.cmd([[
+--     nnoremap <silent> <leader> :WhichKey '<Space>'<CR>
+-- 
+--     " By default timeoutlen is 1000 ms
+--     set timeoutlen=500
+-- 
+--     nnoremap <silent> <leader>      :<c-u>WhichKey '<Space>'<CR>
+--     nnoremap <silent> <localleader> :<c-u>WhichKey  '<Tab>'<CR>
+-- 
+--     " let g:which_key_map['w'] = {
+--     "   \ 'name' : '+windows' ,
+--     "   \ 'w' : ['<C-W>w'     , 'other-window']          ,
+--     "   \ 'd' : ['<C-W>c'     , 'delete-window']         ,
+--     "   \ '-' : ['<C-W>s'     , 'split-window-below']    ,
+--     "   \ '|' : ['<C-W>v'     , 'split-window-right']    ,
+--     "   \ '2' : ['<C-W>v'     , 'layout-double-columns'] ,
+--     "   \ 'h' : ['<C-W>h'     , 'window-left']           ,
+--     "   \ 'j' : ['<C-W>j'     , 'window-below']          ,
+--     "   \ 'l' : ['<C-W>l'     , 'window-right']          ,
+--     "   \ 'k' : ['<C-W>k'     , 'window-up']             ,
+--     "   \ 'H' : ['<C-W>5<'    , 'expand-window-left']    ,
+--     "   \ 'J' : [':resize +5'  , 'expand-window-below']   ,
+--     "   \ 'L' : ['<C-W>5>'    , 'expand-window-right']   ,
+--     "   \ 'K' : [':resize -5'  , 'expand-window-up']      ,
+--     "   \ '=' : ['<C-W>='     , 'balance-window']        ,
+--     "   \ 's' : ['<C-W>s'     , 'split-window-below']    ,
+--     "   \ 'v' : ['<C-W>v'     , 'split-window-below']    ,
+--     "   \ '?' : ['Windows'    , 'fzf-window']            ,
+--     "   \ }
+-- ]])
+
+-- TODO `timeoutlen` should probably only be set once in the entire config, do it here? (?)
