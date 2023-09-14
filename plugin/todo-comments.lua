e0ecce45b21c765a45fdc8962490d6e2dd837ae9
Vincent C. Mader <vincent.mader@icloud.com>
2023-09-14 16:57:58 +0200

    MINOR: Add `todo!` keyword to `todo-comments` to show Rust todos

--- plugin/todo-comments.lua
+++ plugin/todo-comments.lua
@@ -9,7 +9,7 @@ require("todo-comments").setup({
         alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, -- a set of other keywords that all map to this FIX keywords
         -- signs = false, -- configure signs for some keywords individually
       },
-      TODO = { icon = " ", color = "info" },
+      TODO = { icon = " ", color = "info", alt = { "todo!" } },
       HACK = { icon = " ", color = "warning" },
       WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
       PERF = { icon = "󱄟 ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
